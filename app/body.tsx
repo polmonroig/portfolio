65c6636e5afc049ffd5b96001068fd428c2596f7
Pol Monroig <hello@polmonroig.com>
2025-10-12 20:02:23 +0200

    Moved body layout to client component

---
+++ app/body.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+
+export const AppBody = ({
+                          children,
+                        }: Readonly<{
+  children: React.ReactNode;
+}>) => {
+
+  const logoWidth = 230 * 0.2;
+  const logoHeight = 230 * 0.2;
+
+  return (
+    <div>
+      <header className=" w-full text-center bg-black text-white fixed top-0">
+        <div className={"inline-block"}>
+          <div className={"absolute top-4 left-4"}>
+            <img width={logoWidth} height={logoHeight} src={"/logo-white-bg-transparent.webp"} alt={"logo"}></img>
+          </div>
+          <div
+            className="flex flex-row gap-12 mx-auto text-lg text-center mx-auto mt-4 pt-4 pb-8 font-strong uppercase">
+            <Link href={"/"} className={"hover:underline decoration-4 text-center block w-24"}>Home</Link>
+            <Link href={"/about"} className={"hover:underline  decoration-4 text-center block w-24"}>About</Link>
+            <Link href={"/projects"} className={"hover:underline decoration-4  block w-24 text-center"}>Projects</Link>
+            <Link href={"https://blog.pol.company"}
+                  className={"hover:underline decoration-4  block w-24 text-center"}>Blog</Link>
+            <Link href={"mailto:hello@pol.company"}
+                  className={"hover:underline decoration-4  block w-24"}>Contact</Link>
+          </div>
+        </div>
+      </header>
+      {children}
+      <footer className={"fixed left-0 bottom-0 w-full text-center "}>
+        <div className="hover:underline-offset-4 py-6">
+          Made with ❤️ by Pol Monroig Company
+        </div>
+      </footer>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono, Alfa_Slab_One, Pacifico} from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
-import Link from "next/link";
+import {AppBody} from "@/app/body";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,36 +36,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const logoWidth = 230 * 0.2;
-  const logoHeight = 230 * 0.2;
-
-
-
   return (
     <html lang="en">
     <body
       className={`${geistSans.variable} ${geistMono.variable} ${alfaSlabOneFont.variable} ${pacificoFont.variable} antialiased bg-texture`}
     >
-    <header className=" w-full text-center bg-black text-white fixed top-0">
-      <div className={"inline-block"}>
-        <div className={"absolute top-4 left-4"}>
-          <img width={logoWidth} height={logoHeight} src={"/logo-white-bg-transparent.webp"} alt={"logo"}></img>
-        </div>
-        <div className="flex flex-row gap-12 mx-auto text-lg text-center mx-auto mt-4 pt-4 pb-8 font-strong uppercase">
-          <Link href={"/"} className={"hover:underline decoration-4 text-center block w-24"}>Home</Link>
-          <Link href={"/about"} className={"hover:underline  decoration-4 text-center block w-24"}>About</Link>
-          <Link href={"/projects"} className={"hover:underline decoration-4  block w-24 text-center"}>Projects</Link>
-          <Link href={"https://blog.pol.company"} className={"hover:underline decoration-4  block w-24 text-center"}>Blog</Link>
-          <Link href={"mailto:hello@pol.company"} className={"hover:underline decoration-4  block w-24"}>Contact</Link>
-        </div>
-      </div>
-    </header>
-    {children}
-    <footer className={"fixed left-0 bottom-0 w-full text-center "}>
-      <div className="hover:underline-offset-4 py-6">
-        Made with ❤️ by Pol Monroig Company
-      </div>
-    </footer>
+      <AppBody children={children}/>
     </body>
     <GoogleAnalytics gaId="G-1N9YMC8PZ2" />
     </html>
