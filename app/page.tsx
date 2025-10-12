2741c82f5fcfccfc5cbc6fd068119fb816fb60bf
Pol Monroig <hello@polmonroig.com>
2025-10-12 13:02:03 +0200

    Added header text animation

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,39 @@ import {useEffect, useState} from "react";
 export default function Home() {
 
   const [headerIndex, setHeaderIndex] = useState<number>(0);
+  const [translateClass, setTranslateClass] = useState<string>("transition-transform duration-500  ease-in-out -translate-y-full");
 
 
   const headerList = ["Freelance Developer", "Web Developer",
                       "Data Scientist", "Computer Engineer"];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setHeaderIndex((prevIndex) => (prevIndex + 1) % headerList.length);
-    }, 1500)
+  const triggerHeaderChange = () => {
 
-    return () => clearInterval(interval);
+    setTimeout(() => {
+      setTranslateClass("-translate-y-full");
+      setTimeout(() => {
+        triggerFadeInText();
+        setHeaderIndex((prevIndex) => (prevIndex + 1) % headerList.length);
+        triggerHeaderChange();
+      }, 100)
+    }, 2700)
+  }
+
+  const triggerFadeOutText = () => {
+    setTranslateClass("transition-transform duration-500  ease-in-out -translate-y-full translate-y-full");
+  }
+
+  const triggerFadeInText = () => {
+    setTranslateClass("transition-transform duration-500  ease-in-out -translate-y-full translate-y-0");
+    setTimeout(() => {
+      triggerFadeOutText();
+    }, 2000)
+  }
+
+  useEffect(() => {
+    triggerFadeInText();
+    triggerHeaderChange();
+
   }, [])
 
 
@@ -27,8 +49,10 @@ export default function Home() {
             Pol Company
           </div>
           <div/>
-          <div className="text-5xl mb-12 text-center mx-auto">
-            {headerList[headerIndex]}
+          <div className="text-5xl mb-12 text-center mx-auto relative text-nowrap overflow-hidden">
+            <div className={`overflow-hidden  text-center mx-auto  ${translateClass}`}>
+              {headerList[headerIndex]}
+            </div>
           </div>
           <div className="text-2xl/10 mt-5 text-center mx-auto" style={{maxWidth: "600px"}}>
             <mark className={"bg-black text-white"}>I am a full-spectrum digital consultant and developer, creating data-powered applications and scalable web
