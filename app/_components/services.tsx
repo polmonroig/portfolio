0f18a354abe07f695d80199e3540341d758b9c96
Pol Monroig <hello@polmonroig.com>
2025-11-08 22:55:43 +0100

    "Refactored services component and updated related styles: improved layout, added line elements, adjusted gradient and sizing, and enhanced code readability."

--- app/_components/services.tsx
+++ app/_components/services.tsx
@@ -1,5 +1,5 @@
 const ServiceItem = (props: {
-    number: number, title: string, serviceList: string[],
+    number: string, title: string, serviceList: string[],
     side: string,
     src: string
 }) => {
@@ -10,8 +10,14 @@ const ServiceItem = (props: {
             {
                 props.side === "left" ?
                     <div className={"component-services-item-inner"}>
-                        <div>
-                            <div>{props.number} <span>---</span> {props.title}</div>
+                        <div className={"component-services-item-inner-text"}>
+                            <div className={"layout-flex-row style-paragraph"}>
+                                <div className={"layout-margin-y-auto"}>{props.number}</div>
+                                <div className={"element-line-small"}></div>
+                                <div className={"layout-margin-y-auto"}>
+                                    {props.title}
+                                </div>
+                            </div>
                             <ul>
                                 {props.serviceList.map((service, index) => (
                                     <li key={index} className={"style-h3"}>{service}</li>
@@ -22,9 +28,15 @@ const ServiceItem = (props: {
                     </div>
                     :
                     <div className={"component-services-item-inner"}>
-                        <img src={props.src} alt={"service-image"} className={"component-services-item-image"} />
-                        <div>
-                            <div>{props.number} <span>---</span> {props.title}</div>
+                        <img src={props.src} alt={"service-image"} className={"component-services-item-image"}/>
+                        <div className={"component-services-item-inner-text"}>
+                            <div className={"layout-flex-row style-paragraph"}>
+                                <div className={"layout-margin-y-auto"}>{props.number}</div>
+                                <div className={"element-line-small"}></div>
+                                <div className={"layout-margin-y-auto"}>
+                                    {props.title}
+                                </div>
+                            </div>
                             <ul>
                                 {props.serviceList.map((service, index) => (
                                     <li key={index} className={"style-h3"}>{service}</li>
@@ -43,20 +55,23 @@ export const Services = () => {
 
     return (
         <div className={"component-services"}>
-            <div className={"style-h2 style-gradient-text mx-auto"}>
-                Services
+            <div className={"component-services-header"}>
+                <div className={"style-gradient-text"}>
+                    Services
+                </div>
+                <div className={"element-line-large"}></div>
             </div>
-            <ServiceItem number={1} title="Web Development & Interactive Experiences"
+            <ServiceItem number={"01"} title="Web Development & Interactive Experiences"
                          src={"images/services-01.jpg"}
                          side={"left"}
                          serviceList={["Full-Stack", "Frontend", "Backend Architecture",
                              "3D Web with WebGL", "Scalable APIs"]}/>
-            <ServiceItem number={2} title="Machine Learning & Data Science Solutions"
+            <ServiceItem number={"02"} title="Machine Learning & Data Science Solutions"
                          src={"images/services-02.jpg"}
                          side={"right"}
                          serviceList={["Model", "Development", "Predictive", "Analytics Data",
-                             "Modeling", "Algorithm Design", "Deep Learning"]}/>
-            <ServiceItem number={3} title="Real-Time Data Visualization & Dashboards"
+                             "Bayesian", "Algorithm Design", "Deep Learning"]}/>
+            <ServiceItem number={"03"} title="Real-Time Data Visualization & Dashboards"
                          src={"images/services-03.jpg"}
                          side={"left"}
                          serviceList={["Custom Dashboards", "Real-Time Data",
