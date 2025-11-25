12b8e0928cfc2b9d70edfbf24bb5ff4b4e9a3676
Pol Monroig <hello@polmonroig.com>
2025-11-25 10:19:07 +0100

    Added initial animation text test

--- app/body.tsx
+++ app/body.tsx
@@ -1,13 +1,44 @@
 'use client'
 import Script from 'next/script';
 import {NavBar} from "@/app/_components/navbar";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {SplitText} from "gsap/SplitText";
 
+// Register GSAP plugins properly
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
 export const AppBody = ({
                             children,
                         }: Readonly<{
     children: React.ReactNode;
 }>) => {
+
+    useGSAP(() => {
+
+        let split = SplitText.create('.animation-service-title', {
+            type: 'words'
+        })
+
+        gsap.from(
+            split.words,
+            {
+                duration: 1,
+                stagger: 0.05,
+                autoAlpha: 0,
+                y: 100,
+                scrollTrigger: {
+                    start: 'top center+=100',
+                    trigger: '.animation-service-title',
+                    toggleActions: 'play none none none',
+                    markers: true
+                },
+                // If you want to animate the text content itself, set
+                // text: "Your target text here"
+            }
+        )
+    })
 
 
     return (
@@ -19,7 +50,6 @@ export const AppBody = ({
 }
 
 export const UmamiAnalytics = () => {
-    1
     return <Script async src="https://cloud.umami.is/script.js"
                    data-website-id="e6a04351-01ff-487c-bd4e-e829dde57d3e"></Script>
 }
