3e1ceb6098edb035e1b912998cba2bfcde50d174
Pol Monroig <hello@polmonroig.com>
2025-12-21 17:53:58 +0100

    Added mutation steps SVG for genetic brushes project, and images to project

--- app/projects/genetic_brushes/page.tsx
+++ app/projects/genetic_brushes/page.tsx
@@ -1,5 +1,5 @@
 import {ProjectViewTemplate} from "@/app/_components/projects";
-import {Card, CardLarge, CardText, ResponsiveImage} from "@/app/_components/elements";
+import {Card, CardLarge, CardText, ResponsiveImage, TextHeader} from "@/app/_components/elements";
 
 export default function ProjectView() {
     return (
@@ -13,6 +13,37 @@ export default function ProjectView() {
             mockup={null}
         >
             <>
+                <TextHeader titleLines={["Brushes that grow", "based on previous generations"]}/>
+                <Card
+                    src={"/images/projects/genetic_brushes/mutation_original.webp"}
+                    title={"Original"}
+                    side={"right"}
+                >
+                    In the original each individual is position stochastically and their characteristics are
+                    independent.
+                </Card>
+                <Card
+                    src={"/images/projects/genetic_brushes/mutation_mutation.webp"}
+                    title={"Mutation"}
+                    side={"left"}
+                >
+                    In the next generation some attributes of the samples may variate.
+                </Card>
+                <Card
+                    src={"/images/projects/genetic_brushes/mutation_crossover.webp"}
+                    title={"Crossover"}
+                    side={"right"}
+                >
+                    After the individual variations have been made, a merge between different brushes is applied.
+                </Card>
+                <Card
+                    src={"/images/projects/genetic_brushes/mutation_selection.webp"}
+                    title={"Selection"}
+                    side={"left"}
+                >
+                    Finally based on the position and size of each brush, an importance selection
+                    is made and most individuals are removed.
+                </Card>
             </>
         </ProjectViewTemplate>
     )
