cbbc11431b55e4834c42d234471e4ff31a92a395
Pol Monroig <hello@polmonroig.com>
2025-12-21 12:13:28 +0100

    Added genetic brushes cover image and project section

---
+++ app/projects/genetic_brushes/page.tsx
@@ -0,0 +1,19 @@
+import {ProjectViewTemplate} from "@/app/_components/projects";
+import {Card, CardLarge, CardText, ResponsiveImage} from "@/app/_components/elements";
+
+export default function ProjectView() {
+    return (
+        <ProjectViewTemplate
+            id={"genetic_brushes"}
+            description={"Artificial Genetic Algorithms system that generates unique art pieces that simulate brushes."}
+            year={"2024"}
+            client={"Mall"}
+            department={"Technology"}
+            location={"Madrid, ES"}
+            mockup={null}
+        >
+            <>
+            </>
+        </ProjectViewTemplate>
+    )
+}
